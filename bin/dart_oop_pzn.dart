2f752298c3eca5492756544cd57872cb17a5dbb0
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-07-15 17:40:08 +0700

    init commit

---
+++ bin/dart_oop_pzn.dart
@@ -0,0 +1,5 @@
+import 'package:dart_oop_pzn/dart_oop_pzn.dart' as dart_oop_pzn;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_oop_pzn.calculate()}!');
+}
---
+++ test/dart_oop_pzn_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_oop_pzn/dart_oop_pzn.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
