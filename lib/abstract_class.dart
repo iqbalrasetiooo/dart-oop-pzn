d28ec5b87002ab724d27ba358063eefbb556f7c8
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-07-25 22:27:06 +0700

    add new abstract_class.dart file

---
+++ lib/abstract_class.dart
@@ -0,0 +1,6 @@
+import 'data/location_abstract_class.dart';
+
+void main() {
+  var city = City('Indramayu');
+  // var location = Location(); // error, karena abstract class tidak bisa dibuat objek
+}
